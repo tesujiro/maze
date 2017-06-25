1d02ed4ae8805a1c022a45dd5b5499bbc729934a
tesujiro <tesujiro@gmail.com>
2017-06-25 19:57:42 +0900

    refactotring, add width height parameters

--- maze.go
+++ maze.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
@@ -9,8 +10,8 @@ import (
 	"time"
 )
 
-const Width = 31
-const Height = 21
+//const Width = 51
+//const Height = 31
 
 const (
 	WALL   = "#"
@@ -26,39 +27,69 @@ type Point struct {
 	y int
 }
 
-type Maze [][]bool
+func (p2 Point) opposite(p1 Point) Point {
+	return Point{2*p1.x - p2.x, 2*p1.y - p2.y}
+}
 
-func NewMaze() Maze {
-	m := make([][]bool, Width)
-	for i := range m {
-		m[i] = make([]bool, Height)
+func getPointAtRandom(in []Point) []Point {
+	if len(in) == 1 {
+		return []Point{in[0]}
+	} else {
+		i := rand.Intn(len(in))
+		newList := []Point{}
+		for k, p := range in {
+			if k != i {
+				newList = append(newList, p)
+			}
+		}
+		return append(getPointAtRandom(newList), in[i])
 	}
-	return Maze(m)
 }
 
-func (m Maze) setWall(p Point) {
-	m[p.x][p.y] = true
+//type Maze [][]bool
+type Maze struct {
+	width  int
+	height int
+	data   [][]bool
+	start  Point
+	finish Point
+}
+
+func NewMaze(w, h int) *Maze {
+	m := make([][]bool, w)
+	for i := range m {
+		m[i] = make([]bool, h)
+	}
+	return &Maze{
+		width:  w,
+		height: h,
+		data:   m,
+	}
+}
+
+func (m *Maze) setWall(p Point) {
+	m.data[p.x][p.y] = true
 	//m.print()
 }
 
-func (m Maze) setRoad(p Point) {
-	m[p.x][p.y] = false
+func (m *Maze) setRoad(p Point) {
+	m.data[p.x][p.y] = false
 	//m.print()
 }
 
-func (m Maze) point(p Point) bool {
-	if p.x < 0 || p.x >= Width || p.y < 0 || p.y >= Height {
+func (m *Maze) point(p Point) bool {
+	if p.x < 0 || p.x >= m.width || p.y < 0 || p.y >= m.height {
 		return false
 	} else {
-		return m[p.x][p.y]
+		return m.data[p.x][p.y]
 	}
 }
 
-func (m Maze) print() {
+func (m *Maze) print() {
 	fmt.Print("\x1b[H\x1b[2J") // Clear Screen
-	for y := 0; y < Height; y++ {
-		for x := 0; x < Width; x++ {
-			if m.point(Point{x, Height - y - 1}) == true {
+	for y := 0; y < m.height; y++ {
+		for x := 0; x < m.width; x++ {
+			if m.point(Point{x, m.height - y - 1}) == true {
 				fmt.Printf("%."+strconv.Itoa(x%2+1)+"s", strings.Repeat(WALL, 3))
 			} else {
 				fmt.Printf("%."+strconv.Itoa(x%2+1)+"s", strings.Repeat(ROAD, 3))
@@ -68,7 +99,7 @@ func (m Maze) print() {
 	}
 }
 
-func (m Maze) drawLine(p1, p2 Point) {
+func (m *Maze) drawLine(p1, p2 Point) {
 	X, x, Y, y := p1.x, p2.x, p1.y, p2.y
 	if x > X {
 		X, x = p2.x, p1.x
@@ -89,14 +120,14 @@ func (m Maze) drawLine(p1, p2 Point) {
 	}
 }
 
-func (m Maze) drawFrame(p1, p2 Point) {
+func (m *Maze) drawFrame(p1, p2 Point) {
 	m.drawLine(Point{p1.x, p1.y}, Point{p2.x, p1.y})
 	m.drawLine(Point{p2.x, p1.y}, Point{p2.x, p2.y})
 	m.drawLine(Point{p2.x, p2.y}, Point{p1.x, p2.y})
 	m.drawLine(Point{p1.x, p2.y}, Point{p1.x, p1.y})
 }
 
-func (m Maze) nextTo(p Point) []Point {
+func (m *Maze) nextTo(p Point) []Point {
 	var plist []Point
 	nlist := []Point{
 		Point{p.x - 1, p.y - 1},
@@ -109,29 +140,21 @@ func (m Maze) nextTo(p Point) []Point {
 		Point{p.x, p.y - 1},
 	}
 	for _, n := range nlist {
-		if n.x >= 0 && n.x < Width && n.y >= 0 && n.y < Height {
+		if n.x >= 0 && n.x < m.width && n.y >= 0 && n.y < m.height {
 			plist = append(plist, n)
 		}
 	}
 	return plist
 }
 
-func straight(p1, p2, p3 Point) bool {
-	return p2.x == (p1.x+p3.x)/2 && p2.y == (p1.y+p3.y)/2
-}
-
-func opposite(p1, p2 Point) Point {
-	return Point{2*p1.x - p2.x, 2*p1.y - p2.y}
-}
-
-func (m Maze) canPlot(p Point) bool {
+func (m *Maze) canPlot(p Point) bool {
 	if p.x%2 == 1 && p.y%2 == 1 {
 		return false
 	}
 	//fmt.Printf("p.x=%v\tp.y=%v\n", p.x, p.y)
 	for _, n := range m.nextTo(p) {
 		if m.point(n) {
-			if m.point(opposite(p, n)) {
+			if m.point(n.opposite(p)) {
 				//fmt.Printf("\topposite false p=%v\tn=%v\topposite(p,n)=%v\n", p, n, opposite(p, n))
 				return false
 			}
@@ -140,46 +163,24 @@ func (m Maze) canPlot(p Point) bool {
 	return true
 }
 
-func (m Maze) randomPoint() Point {
-	//p := Point{rand.Intn(int(Width/2)) * 2, rand.Intn(int(Height/2)) * 2}
-	//return Point{rand.Intn(Width), rand.Intn(Height)}
-	return Point{rand.Intn(int(Width/2)) * 2, rand.Intn(int(Height/2)) * 2}
+func (m *Maze) randomPoint() Point {
+	//p := Point{rand.Intn(int(m.width/2)) * 2, rand.Intn(int(m.height/2)) * 2}
+	//return Point{rand.Intn(m.width), rand.Intn(m.height)}
+	return Point{rand.Intn(int(m.width/2)) * 2, rand.Intn(int(m.height/2)) * 2}
 }
 
-func (m Maze) plotWallAtRandom() {
-	p := m.randomPoint()
-	if m.canPlot(p) {
-		m.setWall(p)
-	}
-}
-
-func getPointAtRandom(in []Point) []Point {
-	if len(in) == 1 {
-		return []Point{in[0]}
-	} else {
-		i := rand.Intn(len(in))
-		newList := []Point{}
-		for k, p := range in {
-			if k != i {
-				newList = append(newList, p)
-			}
-		}
-		return append(getPointAtRandom(newList), in[i])
-	}
-}
-
-func (m Maze) getWallCandidate(p Point) []Point {
+func (m *Maze) getWallCandidate(p Point) []Point {
 	var result []Point
 	var list []Point = []Point{Point{p.x - 1, p.y}, Point{p.x, p.y + 1}, Point{p.x + 1, p.y}, Point{p.x, p.y - 1}}
 	for _, p := range list {
-		if p.x >= 0 && p.x < Width && p.y >= 0 && p.y < Height && m.point(p) == false {
+		if p.x >= 0 && p.x < m.width && p.y >= 0 && p.y < m.height && m.point(p) == false {
 			result = append(result, p)
 		}
 	}
 	return result
 }
 
-func (m Maze) extendWall(p Point) {
+func (m *Maze) extendWall(p Point) {
 	//fmt.Printf("p.x=%v\tp.y=%v\n", p.x, p.y)
 	for _, wc := range getPointAtRandom(m.getWallCandidate(p)) {
 		//fmt.Printf("\twc.x=%v\twc.y=%v\n", wc.x, wc.y)
@@ -190,17 +191,16 @@ func (m Maze) extendWall(p Point) {
 	}
 }
 
-func (m Maze) makeMaze() {
-	m.drawFrame(Point{0, 0}, Point{Width - 1, Height - 1})
-	//var list [(int(Width/2) + int(Height/2)) * 2 - 4 ]Point
+func (m *Maze) makeMaze() {
+	m.drawFrame(Point{0, 0}, Point{m.width - 1, m.height - 1})
 	var list []Point
-	for i := 2; i < Width-1; i += 2 {
+	for i := 2; i < m.width-1; i += 2 {
 		list = append(list, Point{i, 0})
-		list = append(list, Point{i, Height - 1})
+		list = append(list, Point{i, m.height - 1})
 	}
-	for i := 2; i < Height-1; i += 2 {
+	for i := 2; i < m.height-1; i += 2 {
 		list = append(list, Point{0, i})
-		list = append(list, Point{Width - 1, i})
+		list = append(list, Point{m.width - 1, i})
 	}
 	for _, p := range getPointAtRandom(list) {
 		m.extendWall(p)
@@ -208,14 +208,12 @@ func (m Maze) makeMaze() {
 }
 
 func main() {
+	var width *int = flag.Int("width", 51, "Width of the maze.")
+	var height *int = flag.Int("height", 31, "Height of the maze.")
+	flag.Parse()
+
 	rand.Seed(time.Now().UnixNano())
-	m := NewMaze()
-	/*
-		m.drawFrame(m.randomPoint(), m.randomPoint())
-		for i := 1; i < 10; i++ {
-			m.plotWallAtRandom()
-		}
-	*/
+	m := NewMaze(*width, *height)
 	m.makeMaze()
 	m.print()
 }
